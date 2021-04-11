ebbd15cf2fc0a0280e98f26c7f2bedf0c6d67315
Emily Scudero <emily@MSI.COM>
2021-04-11 15:33:57 -0400

    db associations

--- src/components/apps/timer-apps/TimerApp.js
+++ src/components/apps/timer-apps/TimerApp.js
@@ -1,22 +1,21 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 
 const TimerApp = () => {
-const [secondsElapsed, setSecondsElapsed] = useState(0);
+  const [secondsElapsed, setSecondsElapsed] = useState(0);
 
-     useEffect(() => {
-     let interval = setInterval(() => tick(), 1000);
-     console.log(interval);
-     return() => clearInterval(interval);
- })
+  useEffect(() => {
+    let interval = setInterval(() => tick(), 1000);
+    console.log(interval);
+    return () => clearInterval(interval);
+  });
 
- const tick = () => setSecondsElapsed(secondsElapsed + 1);
-    return(
-        <div>
-            <h1 className="section-title">React Timer</h1>
-            <div>Seconds Elapsed: {secondsElapsed} </div>
-        </div>
-    )
-}
-    
+  const tick = () => setSecondsElapsed(secondsElapsed + 1);
+  return (
+    <div>
+      <h1 className="section-title">React Timer</h1>
+      <div>Seconds Elapsed: {secondsElapsed} </div>
+    </div>
+  );
+};
 
 export default TimerApp;
